7252c83817bf255acd6188ffaab57ea364298475
nathan <nathan@jkt1.ebdesk.com>
2022-12-26 19:25:04 +0700

    style::pencil: delete package not use

--- src/main.py
+++ src/main.py
@@ -1,12 +1,6 @@
 from loguru import logger
-from config.read_env import *
 from distutils.util import strtobool
 
 if __name__ == "__main__":
-
-    if strtobool(coba_aja):
-        logger.info("Coba_Aja: {} ", coba_aja)
-        logger.info("My_Config: {}", my_config)
-    
     logger.info("My message: Hello") 
     print("Hello World")
